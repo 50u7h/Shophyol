778cdbbb5fa1b20984580157bb407407ab33c7ca
50u7h <50u7h@protonmail.com>
2023-01-26 16:58:41 +0300

    brand pagination, search, sort

--- ShophyolProject/ShophyolWebParent/ShophyolBackEnd/src/main/java/com/shophyol/admin/brand/BrandRepository.java
+++ ShophyolProject/ShophyolWebParent/ShophyolBackEnd/src/main/java/com/shophyol/admin/brand/BrandRepository.java
@@ -1,6 +1,9 @@
 package com.shophyol.admin.brand;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.shophyol.common.entity.Brand;
 
@@ -9,5 +12,8 @@ public interface BrandRepository extends JpaRepository<Brand, Integer> {
 	public Long countById(Integer id);
 
 	public Brand findByName(String name);
+
+	@Query("SELECT b FROM Brand b WHERE b.name LIKE %?1%")
+	public Page<Brand> findAll(String keyword, Pageable pageable);
 
 }
--- ShophyolProject/ShophyolWebParent/ShophyolBackEnd/src/main/java/com/shophyol/admin/brand/BrandService.java
+++ ShophyolProject/ShophyolWebParent/ShophyolBackEnd/src/main/java/com/shophyol/admin/brand/BrandService.java
@@ -4,12 +4,18 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.shophyol.common.entity.Brand;
 
 @Service
 public class BrandService {
+
+	public static final int BRANDS_PER_PAGE = 8;
 
 	@Autowired
 	private BrandRepository repo;
@@ -60,5 +66,19 @@ public class BrandService {
 		}
 
 		return "OK";
+	}
+
+	public Page<Brand> listByPage(int pageNum, String sortField, String sortDir, String keyword) {
+		Sort sort = Sort.by(sortField);
+
+		sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+
+		Pageable pageable = PageRequest.of(pageNum - 1, BRANDS_PER_PAGE, sort);
+
+		if (keyword != null) {
+			return repo.findAll(keyword, pageable);
+		}
+
+		return repo.findAll(pageable);
 	}
 }
