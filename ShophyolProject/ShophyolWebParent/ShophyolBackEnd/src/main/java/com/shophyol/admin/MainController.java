d103d78ac4b3bc47197d5e57e92ec78ef932c5d2
50u7h <50u7h@protonmail.com>
2023-02-03 18:55:20 +0300

    product refactoring

--- ShophyolProject/ShophyolWebParent/ShophyolBackEnd/src/main/java/com/shophyol/admin/MainController.java
+++ ShophyolProject/ShophyolWebParent/ShophyolBackEnd/src/main/java/com/shophyol/admin/MainController.java
@@ -1,5 +1,8 @@
 package com.shophyol.admin;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,7 +11,12 @@ public class MainController {
 
 	@GetMapping("")
 	public String viewHomePage() {
-		return "index";
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+			return "login";
+		}
+
+		return "redirect:/";
 	}
 
 	@GetMapping("/login")
