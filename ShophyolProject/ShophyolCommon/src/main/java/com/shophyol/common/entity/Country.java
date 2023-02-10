9eb22352ccb672c8eae291054ec62b29d755aec2
50u7h <50u7h@protonmail.com>
2023-02-10 17:33:59 +0300

    settings - country - rest controller, tests

--- ShophyolProject/ShophyolCommon/src/main/java/com/shophyol/common/entity/Country.java
+++ ShophyolProject/ShophyolCommon/src/main/java/com/shophyol/common/entity/Country.java
@@ -13,7 +13,7 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "countries")
 public class Country {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -34,6 +34,16 @@ public class Country {
 	public Country(String name, String code) {
 		this.name = name;
 		this.code = code;
+	}
+
+	public Country(Integer id, String name, String code) {
+		this.id = id;
+		this.name = name;
+		this.code = code;
+	}
+
+	public Country(String name) {
+		this.name = name;
 	}
 
 	public Integer getId() {
@@ -58,14 +68,6 @@ public class Country {
 
 	public void setCode(String code) {
 		this.code = code;
-	}
-
-	public Set<State> getStates() {
-		return states;
-	}
-
-	public void setStates(Set<State> states) {
-		this.states = states;
 	}
 
 	@Override
