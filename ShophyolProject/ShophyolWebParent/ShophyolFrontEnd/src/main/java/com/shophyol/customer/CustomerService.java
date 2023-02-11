7411643d1b29ac1ae502354c0d03167c766df453
50u7h <50u7h@protonmail.com>
2023-02-12 00:15:51 +0300

    application - send verification email

--- ShophyolProject/ShophyolWebParent/ShophyolFrontEnd/src/main/java/com/shophyol/customer/CustomerService.java
+++ ShophyolProject/ShophyolWebParent/ShophyolFrontEnd/src/main/java/com/shophyol/customer/CustomerService.java
@@ -1,31 +1,55 @@
 package com.shophyol.customer;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.shophyol.common.entity.Country;
 import com.shophyol.common.entity.Customer;
 import com.shophyol.setting.CountryRepository;
 
+import net.bytebuddy.utility.RandomString;
+
 @Service
 public class CustomerService {
 
 	@Autowired
 	private CountryRepository countryRepo;
-	
+
 	@Autowired
 	private CustomerRepository customerRepo;
 
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
 	public List<Country> listAllCountries() {
-		
+
 		return countryRepo.findAllByOrderByNameAsc();
 	}
 
 	public boolean isEmailUnique(String email) {
 		Customer customer = customerRepo.findByEmail(email);
-		
+
 		return customer == null;
+	}
+
+	public void registerCustomer(Customer customer) {
+		encodePassword(customer);
+		customer.setEnabled(false);
+		customer.setCreatedTime(new Date());
+
+		String randomCode = RandomString.make(64);
+		customer.setVerificationCode(randomCode);
+
+		customerRepo.save(customer);
+
+	}
+
+	private void encodePassword(Customer customer) {
+		String encodedPassword = passwordEncoder.encode(customer.getPassword());
+		customer.setPassword(encodedPassword);
 	}
 }
--- ShophyolProject/ShophyolWebParent/ShophyolFrontEnd/src/main/java/com/shophyol/security/WebSecurityConfig.java
+++ ShophyolProject/ShophyolWebParent/ShophyolFrontEnd/src/main/java/com/shophyol/security/WebSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,4 +28,8 @@ public class WebSecurityConfig {
 		return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
