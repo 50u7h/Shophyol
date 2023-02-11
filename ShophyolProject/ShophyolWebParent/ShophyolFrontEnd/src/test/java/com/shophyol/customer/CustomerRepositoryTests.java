a8a1beb36f3d74b436978db9bc736da13da9d53b
50u7h <50u7h@protonmail.com>
2023-02-11 21:47:48 +0300

    application - customer - entity, repository, unit tests

---
+++ ShophyolProject/ShophyolWebParent/ShophyolFrontEnd/src/test/java/com/shophyol/customer/CustomerRepositoryTests.java
@@ -0,0 +1,145 @@
+package com.shophyol.customer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Date;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.shophyol.common.entity.Country;
+import com.shophyol.common.entity.Customer;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class CustomerRepositoryTests {
+
+	@Autowired
+	private CustomerRepository repo;
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Test
+	public void testCreateCustomer1() {
+		Integer countryId = 1;
+		Country country = entityManager.find(Country.class, countryId);
+
+		Customer customer = new Customer();
+		customer.setCountry(country);
+		customer.setFirstName("Samet");
+		customer.setLastName("Guney");
+		customer.setPassword("123456789");
+		customer.setEmail("samet@guney.com");
+		customer.setPhoneNumber("123-456-7890");
+		customer.setAddressLine1("1234 Test");
+		customer.setCity("Istanbul");
+		customer.setState("Istanbul");
+		customer.setPostalCode("123456");
+		customer.setCreatedTime(new Date());
+
+		Customer savedCustomer = repo.save(customer);
+
+		assertThat(savedCustomer).isNotNull();
+		assertThat(savedCustomer.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testCreateCustomer2() {
+		Integer countryId = 3;
+		Country country = entityManager.find(Country.class, countryId);
+
+		Customer customer = new Customer();
+		customer.setCountry(country);
+		customer.setFirstName("Sanya");
+		customer.setLastName("Lad");
+		customer.setPassword("password456");
+		customer.setEmail("sanya.lad2020@gmail.com");
+		customer.setPhoneNumber("02224928052");
+		customer.setAddressLine1("173 , A-, Shah & Nahar Indl.estate, Sunmill Road");
+		customer.setAddressLine2("Dhanraj Mill Compound, Lower Parel (west)");
+		customer.setCity("Mumbai");
+		customer.setState("Maharashtra");
+		customer.setPostalCode("400013");
+		customer.setCreatedTime(new Date());
+
+		Customer savedCustomer = repo.save(customer);
+
+		assertThat(savedCustomer).isNotNull();
+		assertThat(savedCustomer.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testListCustomers() {
+		Iterable<Customer> customers = repo.findAll();
+		customers.forEach(System.out::println);
+
+		assertThat(customers).hasSizeGreaterThan(1);
+	}
+
+	@Test
+	public void testUpdateCustomer() {
+		Integer customerId = 2;
+		String lastName = "Stanfield";
+
+		Customer customer = repo.findById(customerId).get();
+		customer.setLastName(lastName);
+		customer.setEnabled(true);
+
+		Customer updatedCustomer = repo.save(customer);
+		assertThat(updatedCustomer.getLastName()).isEqualTo(lastName);
+	}
+
+	@Test
+	public void testGetCustomer() {
+		Integer customerId = 2;
+		Optional<Customer> findById = repo.findById(customerId);
+
+		assertThat(findById).isPresent();
+
+		Customer customer = findById.get();
+		System.out.println(customer);
+	}
+
+	@Test
+	public void testDeleteCustomer() {
+		Integer customerId = 2;
+		repo.deleteById(customerId);
+
+		Optional<Customer> findById = repo.findById(customerId);
+		assertThat(findById).isNotPresent();
+	}
+
+	@Test
+	public void testFindByEmail() {
+		String email = "samet@guney.com";
+		Customer customer = repo.findByEmail(email);
+
+		assertThat(customer).isNotNull();
+		System.out.println(customer);
+	}
+
+	@Test
+	public void testFindByVerificationCode() {
+		String code = "123456";
+		Customer customer = repo.findByVerificationCode(code);
+
+		assertThat(customer).isNotNull();
+		System.out.println(customer);
+	}
+
+	@Test
+	public void testEnableCustomer() {
+		Integer customerId = 1;
+		repo.enable(customerId);
+
+		Customer customer = repo.findById(customerId).get();
+		assertThat(customer.isEnabled()).isTrue();
+	}
+}
