915a68c314d9c6dfeb5db095cab55911da4e8a72
50u7h <50u7h@protonmail.com>
2023-02-14 11:50:18 +0300

    customer - login, logout

--- ShophyolProject/ShophyolWebParent/ShophyolFrontEnd/src/main/java/com/shophyol/security/WebSecurityConfig.java
+++ ShophyolProject/ShophyolWebParent/ShophyolFrontEnd/src/main/java/com/shophyol/security/WebSecurityConfig.java
@@ -2,9 +2,11 @@ package com.shophyol.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,7 +18,9 @@ public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-		http.authorizeHttpRequests().anyRequest().permitAll();
+		http.authorizeHttpRequests().requestMatchers("/customer").authenticated().anyRequest().permitAll().and()
+				.formLogin().loginPage("/login").usernameParameter("email").permitAll().and().logout().permitAll().and()
+				.rememberMe();
 
 		http.headers().frameOptions().sameOrigin();
 
@@ -31,5 +35,20 @@ public class WebSecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new CustomerUserDetailsService();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+
+		return authProvider;
 	}
 }
