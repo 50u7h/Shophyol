5122680cafb0a13e342e78ef2416126de88568b7
50u7h <50u7h@protonmail.com>
2023-02-15 17:41:53 +0300

    cart - update quantity

--- ShophyolProject/ShophyolWebParent/ShophyolFrontEnd/src/main/java/com/shophyol/shoppingcart/ShoppingCartService.java
+++ ShophyolProject/ShophyolWebParent/ShophyolFrontEnd/src/main/java/com/shophyol/shoppingcart/ShoppingCartService.java
@@ -8,12 +8,18 @@ import org.springframework.stereotype.Service;
 import com.shophyol.common.entity.CartItem;
 import com.shophyol.common.entity.Customer;
 import com.shophyol.common.entity.Product;
+import com.shophyol.product.ProductRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
+@Transactional
 public class ShoppingCartService {
 
 	@Autowired
 	private CartItemRepository cartRepo;
+	@Autowired
+	private ProductRepository productRepo;
 
 	public Integer addProduct(Integer productId, Integer quantity, Customer customer) throws ShoppingCartException {
 
@@ -43,6 +49,17 @@ public class ShoppingCartService {
 	}
 
 	public List<CartItem> listCartItems(Customer customer) {
+
 		return cartRepo.findByCustomer(customer);
 	}
+
+	public float updateQuantity(Integer productId, Integer quantity, Customer customer) {
+
+		cartRepo.updateQuantity(quantity, customer.getId(), productId);
+		Product product = productRepo.findById(productId).get();
+		float subtotal = product.getDiscountPrice() * quantity;
+
+		return subtotal;
+	}
+
 }
